d62854f179ee1e70a720fd32b2aff4042e8cb7aa
VT7org <glitchnaxusofficial@gmail.com>
2025-06-06 14:40:50 +0530

    Update ping.py

--- TEAMZYRO/plugins/tools/ping.py
+++ TEAMZYRO/plugins/tools/ping.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from pyrogram.types import InlineKeyboardMarkup
 from config import *
 from TEAMZYRO import app
 from TEAMZYRO.core.call import ZYRO
@@ -12,13 +12,12 @@ from TEAMZYRO.utils.inline import supp_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping", "alive"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
-        caption=_["ping_1"].format(app.mention),
+    response = await message.reply_text(
+        _["ping_1"].format(app.mention)
     )
     pytgping = await ZYRO.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
