5cf077668a073b2dcc5c5bb24d46f4b2935dbf82
VT7org <glitchnaxusofficial@gmail.com>
2025-06-22 22:09:18 +0530

    Update misc.py

--- TEAMZYRO/misc.py
+++ TEAMZYRO/misc.py
@@ -39,7 +39,7 @@ XCB = [
 def dbb():
     global db
     db = {}
-    LOGGER(__name__).info(f"𝗗𝗔𝗧𝗔𝗕𝗔𝗦𝗘 𝗟𝗢𝗔𝗗 𝗕𝗔𝗕𝗬🍫........")
+    LOGGER(__name__).info(f"Database Initialised........")
 
 
 async def sudo():
@@ -58,7 +58,7 @@ async def sudo():
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"𝗦𝗨𝗗𝗢 𝗨𝗦𝗘𝗥 𝗗𝗢𝗡𝗘✨🎋.")
+    LOGGER(__name__).info(f"Sudoers Loaded Successfully...")
 
 
 def heroku():
