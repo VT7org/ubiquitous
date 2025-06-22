b325df189ff5305d0dc90938d48c5c5ffe5eab30
VT7org <glitchnaxusofficial@gmail.com>
2025-06-22 22:00:44 +0530

    Update keyboard.py

--- TEAMZYRO/utils/keyboard.py
+++ TEAMZYRO/utils/keyboard.py
@@ -1,9 +1,8 @@
 #--------------made by @xeno_kakarot (team zyro) -------------×
-from pykeyboard import InlineKeyboard
+from pyrogram.types import InlineKeyboardMarkup as InlineKeyboard
 from pyrogram.types import InlineKeyboardButton as Ikb
 
 from .functions import get_urls_from_text as is_url
-
 
 def keyboard(buttons_list, row_width: int = 2):
     buttons = InlineKeyboard(row_width=row_width)
@@ -17,7 +16,6 @@ def keyboard(buttons_list, row_width: int = 2):
     ]
     buttons.add(*data)
     return buttons
-
 
 def ikb(data: dict, row_width: int = 2):
     return keyboard(data.items(), row_width=row_width)
